2f2e8710159c9cbd6e03b188f50833283faa15e7
faisalkhalid <k112023@nu.edu.pk>
2017-10-12 12:02:00 +0400

    Initial Commit

---
+++ Coffee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coffee
+//
+//  Created by faisal khalid on 10/12/17.
+//  Copyright © 2017 Sharjah Cooperative Society. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
